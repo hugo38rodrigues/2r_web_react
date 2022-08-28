1fa45026514b8b850a84b33f61257f27fed5f0bd
DOLIP7438 <t.noellardin@gmail.com>
2022-08-28 12:38:50 +0200

    ajout de la methode add_user et securisation de la connexion

--- back/bdd/data_base.py
+++ back/bdd/data_base.py
@@ -1,22 +1,28 @@
+import pymysql
+import sqlalchemy.exc
 from sqlalchemy import create_engine, exc
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database, drop_database
 #from sqlalchemy_utils.functions.database import drop_database
 #from sqlalchemy.orm import sessionmaker
-from .table import Base, Privilege
-#from .ReadFile import *
-#from .DbRequest import DbRequest
-#from .init.initData import initData
+from .table import Base, Privilege, Categorie
+from .requests_data_base import Requests_Data_Base
 
 DEBUG = True
 
 
-class Data_Base:
+class Data_Base(Requests_Data_Base):
 
     __instance = None
 
     def __init__(self):
-        self.engine = create_engine(f"mysql+pymysql://root:root@localhost/BDD_2R", echo=True)
+        self.engine = create_engine("mysql+pymysql://root:root@localhost/BDD_2R", echo=True)
+        try:
+            self.engine.connect()
+        except sqlalchemy.exc.SQLAlchemyError:
+            print("Pas de BASE DE DONNEE")
+            exit(0)
+
 
         if DEBUG:
             drop_database(self.engine.url)
@@ -26,6 +32,7 @@ class Data_Base:
             Base.metadata.create_all(self.engine)
 
         self.session = sessionmaker(bind=self.engine)()
+        Requests_Data_Base.__init__(Requests_Data_Base, self.session)
 
     @classmethod
     def get_instance(self):
@@ -40,5 +47,15 @@ class Data_Base:
             try:
                 self.session.commit()
             except Exception as e:
+                print(e)
                 self.session.rollback()
 
+    def init_category(self):
+        with self.session.begin():
+            for cat in ["bouffe", "sport", "media", "livre"]:
+                self.session.add(Categorie(nom=cat))
+            try:
+                self.session.commit()
+            except Exception as e:
+                print(e)
+                self.session.rollback()
