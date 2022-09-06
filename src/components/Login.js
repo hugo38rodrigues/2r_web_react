7afec6a6b58a6aa18a2c5071ef3204220697c20b
Xwdrblue <hugo38.rodrigues@gmail.com>
2022-09-06 16:50:14 +0200

    add request post for add user

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,46 @@
 import React from "react";
-// ! ecrire le code pour envoyer le login 
+import axios from "axios";
+import { useState } from "react";
+
 const Login = () => {
+  const [mdp, setMdp] = useState("");
+  const [pseudo, setPseudo] = useState("");
+
+  axios
+    .post("/connexion", {
+      MDP: mdp,
+      Pseudo: pseudo,
+    })
+    .then(function (response) {
+      // ! remplacer le "console.log" par un test de vérification d'un user et ajouter les pages utilisateurs
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
   return (
     <div className="login">
-    <form method="post">
-      <h2>Connexion</h2>
-      <br/> <br/>
-      <label>Pseudo</label>
-      <br />
-      <input type="text" />
-      <br />
-      <br/>
-      <label>Mot de passe</label>
-      <br />
-      <input type="password" name="" id="" />
-      <br/>
-      <br/>
-
-      <button type="submit">Connexion</button>
-    </form>
+      <form method="post">
+        <h2>Connexion</h2>
+        <br /> <br />
+        <label>Pseudo</label>
+        <br />
+        <input type="text" onChange={(e) => setPseudo(e.target.value)} />
+        <br />
+        <br />
+        <label>Mot de passe</label>
+        <br />
+        <input
+          type="password"
+          onChange={(e) => setMdp(e.target.value)}
+          name=""
+          id=""
+        />
+        <br />
+        <br />
+        <button type="submit">Connexion</button>
+      </form>
     </div>
   );
 };
