8a1fbb68f13ea202b4a3acbcd25b30872b3ff1fe
Xwdrblue <hugo38.rodrigues@gmail.com>
2022-09-09 09:26:11 +0200

    add axios method

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,36 +6,49 @@ const Login = () => {
   const [mdp, setMdp] = useState("");
   const [pseudo, setPseudo] = useState("");
 
-  axios
-    .post("/connexion", {
-      MDP: mdp,
-      Pseudo: pseudo,
-    })
-    .then(function (response) {
-      // ! remplacer le "console.log" par un test de vérification d'un user et ajouter les pages utilisateurs
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  const handlesubmit = () => {
+    console.log(mdp);
+    console.log(pseudo);
+    const data_connexion = {
+      pseudo: pseudo,
+      mdp: mdp,
+    };
+
+    axios
+      .post("http://127.0.0.1:5000/connexion", data_connexion)
+
+      .catch((error) => console.log("Error: ", error));
+  };
+  // axios
+  //   .post("http://localhost:5000/connexion", JSON.stringify(connexionData))
+  //   .then(function (response) {
+  //     // ! remplacer le "console.log" par un test de vérification d'un user et ajouter les pages utilisateurs
+  //     console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
 
   return (
     <div className="login">
-      <form method="post">
+      <form onSubmit={handlesubmit}>
         <h2>Connexion</h2>
         <br /> <br />
         <label>Pseudo</label>
         <br />
-        <input type="text" onChange={(e) => setPseudo(e.target.value)} />
+        <input
+          type="text"
+          name="pseudo"
+          onChange={(e) => setPseudo(e.target.value)}
+        />
         <br />
         <br />
         <label>Mot de passe</label>
         <br />
         <input
           type="password"
+          name="mdp"
           onChange={(e) => setMdp(e.target.value)}
-          name=""
-          id=""
         />
         <br />
         <br />
